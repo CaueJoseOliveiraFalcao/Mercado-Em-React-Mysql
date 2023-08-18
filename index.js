4274b2f32824983a75c44e42a27ce53f27e10664
Cauê Josê Oliveira Falcão <cauejose007@gmail.com>
2023-08-18 11:20:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv').config();
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.get('/products' , (req,res) => {
     const SqlComandLine = 'select * from products';
